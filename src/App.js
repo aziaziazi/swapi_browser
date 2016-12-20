fc816fe5f2279d6fe2b960d65ab67afc4da84ecd
aziaziazi <aziaziazi@gmail.com>
2016-12-20 12:07:29 +0100

    import and inject tapEventPlugin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// Needed to fix onTouchTap
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
